70fd0b15a048f0ff5e783da3bc8afc8f5fefad2d
Richard Littauer <richard.littauer@gmail.com>
2017-04-03 04:48:51 -0400

    Init

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const replace = require("replace")
+const meow = require('meow');
+const cli = meow(`
+    Usage:
+        $ smart-to-dumb-quotes FILES [options]
+
+    Options:
+        -h, --help         print usage information
+        -v, --version      show version info and exit
+        -r, --recursive    replace recursively
+        -s, --single       only replace single quotes
+        -d, --double       only replace double quotes
+        --debug            Do not be silent
+`, {
+    alias: {
+      h: 'help',
+      v: 'version',
+      r: 'recursive',
+      s: 'single',
+      d: 'double'
+    }
+})
+
+if (!cli.flags.single && !cli.flags.double || !cli.flags.single && cli.flags.double) {
+  replace({
+    regex: '[“”]',
+    replacement: '"',
+    paths: cli.input,
+    recursive: cli.recursive,
+    silent: (cli.debug !== undefined) ? cli.debug : true
+  })
+}
+
+if (!cli.flags.single && !cli.flags.double || cli.flags.single && !cli.flags.double) {
+  replace({
+    regex: "[‘’]",
+    replacement: "'",
+    paths: cli.input,
+    recursive: cli.recursive,
+    silent: (cli.debug !== undefined) ? cli.debug : true
+  })
+}
